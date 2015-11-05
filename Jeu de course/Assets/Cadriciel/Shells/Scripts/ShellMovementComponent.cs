c5a058bc41ff11891b3c2c21f2498850ad944ece
Raphael Lapierre <raph.lapierre@gmail.com>
2015-11-05 14:46:27 -0500

    shell rouge semi fonctionnel

--- Jeu de course/Assets/Cadriciel/Shells/Scripts/ShellMovementComponent.cs
+++ Jeu de course/Assets/Cadriciel/Shells/Scripts/ShellMovementComponent.cs
@@ -2,20 +2,22 @@
 
 class ShellMovementComponent : MonoBehaviour
 {
-    [SerializeField] public float shellVelocity = 60;
+    [SerializeField] private float m_shellVelocity = 60;
+    [SerializeField] private float m_rotationSpeed = 90;
+    [SerializeField] private int   m_maxNumberOfBounces = 1;
+    private int m_currentNumberOfBounces = 0;
+    const int m_vehiclesLayer = 8;
+    Vector3 m_rotationAxis = Vector3.zero;
 
     void Update()
     {
-        //transform.position += transform.forward * Time.deltaTime * shellVelocity;
-
-        // Drop the shell to the ground
         RaycastHit groundCheckRaycast;
         int layerMask = 1 << 11;
 
         // Raycast below the projectile to follow the ground
         if(Physics.Raycast(transform.position, -Vector3.up, out groundCheckRaycast, Mathf.Infinity, layerMask))
         {
-            transform.position -= Vector3.up * (groundCheckRaycast.distance - transform.collider.bounds.extents.y);
+            transform.position -= Vector3.up * Mathf.Min((groundCheckRaycast.distance - transform.collider.bounds.extents.y), 0.05f);
         }
         else if(Physics.Raycast(transform.position, Vector3.up, out groundCheckRaycast, Mathf.Infinity, layerMask))
         {
@@ -28,11 +30,29 @@ class ShellMovementComponent : MonoBehaviour
         {
             transform.position -= Vector3.forward * (groundCheckRaycast.distance - transform.collider.bounds.extents.x);
         }
+
+        // Give a slight turn to the shell if it has a target
+        if(Target != null)
+        {
+            Vector3 distanceToTarget = Vector3.Normalize(Target.transform.position - transform.position);
+            Vector3 projectileDirection = transform.forward;
+
+            // Apply rotation while vectors are not parallel
+            if(Vector3.Dot(distanceToTarget, projectileDirection) < distanceToTarget.magnitude * projectileDirection.magnitude)
+            {
+                if(m_rotationAxis == Vector3.zero)
+                {
+                    m_rotationAxis = Vector3.Normalize(Vector3.Cross(projectileDirection, distanceToTarget));
+                }
+
+                print("Turning to " + Target.name);
+                rigidbody.velocity = Quaternion.AngleAxis(m_rotationSpeed * Time.deltaTime, m_rotationAxis) * rigidbody.velocity;
+            }
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        print(collision.gameObject.name);
         Rigidbody shell = GetComponent<Rigidbody>();
 
         if(collision.gameObject.name == "Track")
@@ -40,18 +60,37 @@ class ShellMovementComponent : MonoBehaviour
             return;
         }
 
+        // Destroy projectile on collision with a vehicle
+        if((collision.gameObject.layer & m_vehiclesLayer) > 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         foreach(ContactPoint contact in collision.contacts)
         {
             transform.forward = transform.forward - 2 * Vector3.Dot(transform.forward, contact.normal) * contact.normal;
             transform.forward = Vector3.Normalize(new Vector3(transform.forward.x, 0.0f, transform.forward.z));
-            shell.velocity = transform.forward * shellVelocity;
+            shell.velocity = transform.forward * m_shellVelocity;
+
+            if(++m_currentNumberOfBounces > m_maxNumberOfBounces)
+            {
+                Destroy(gameObject);
+            }
+
             return;
         }
     }
 
     public float GetMaxShellVelocity {
         get {
-            return shellVelocity;
+            return m_shellVelocity;
         }
+    }
+
+    public GameObject Target
+    {
+        get;
+        set;
     }
 }
--- Jeu de course/Assets/Sample Assets/Vehicles/Car/Scripts/CarProjectileLauncher.cs
+++ Jeu de course/Assets/Sample Assets/Vehicles/Car/Scripts/CarProjectileLauncher.cs
@@ -10,6 +10,7 @@ class CarProjectileLauncher : MonoBehaviour
     public Rigidbody greenShell;
 
     [SerializeField] private float frontOfVehiculeFireOffset = 10;
+    [SerializeField] private float maxTargetDistance = 10;
 
     public void Fire(bool fired, ShellColors color)
     {
@@ -18,25 +19,72 @@ class CarProjectileLauncher : MonoBehaviour
             switch(color)
             {
                 case ShellColors.Green:
-                    FireShell(greenShell);
+                    FireShell(greenShell, ShellColors.Green);
                     break;
                 case ShellColors.Red:
-                    FireShell(redShell);
+                    FireShell(redShell, ShellColors.Red);
                     break;
                 case ShellColors.Blue:
-                    FireShell(blueShell);
+                    FireShell(blueShell, ShellColors.Blue);
                     break;
             }
         }
     }
 
-    private void FireShell(Rigidbody shell)
+    private void FireShell(Rigidbody shell, ShellColors color)
     {
         Rigidbody firedShell;
         firedShell = Instantiate(shell, transform.position, transform.rotation) as Rigidbody;
         firedShell.transform.position += firedShell.transform.forward * frontOfVehiculeFireOffset;
         firedShell.transform.position += firedShell.transform.up * 0.5f;
         firedShell.velocity = firedShell.transform.forward * firedShell.GetComponent<ShellMovementComponent>().GetMaxShellVelocity;
-        firedShell.GetComponent<ShellMovementComponent>().enabled = true;
+        ShellMovementComponent shellMoveComp = firedShell.GetComponent<ShellMovementComponent>();
+        shellMoveComp.enabled = true;
+
+        switch (color)
+        {
+            case ShellColors.Green:
+                return;
+            case ShellColors.Red:
+            case ShellColors.Blue:
+
+                GameObject cars = GameObject.Find("Cars");
+                GameObject target = null;
+
+                float distanceToTarget = 0.0f;
+                bool first = true;
+
+                foreach(Transform child in cars.transform)
+                {
+                    if(child.gameObject.name == gameObject.name)
+                    {
+                        continue;
+                    }
+
+                    if(first)
+                    {
+                        distanceToTarget = (child.transform.position - gameObject.transform.position).magnitude;
+                        target = child.gameObject;
+                        first = false;                    
+                    }
+
+                    float newDistanceToTarget = (child.position - firedShell.transform.position).magnitude;
+                    if(distanceToTarget > newDistanceToTarget)
+                    {
+                        distanceToTarget = newDistanceToTarget;
+                        target = child.gameObject;
+                    }
+                }
+
+                if(distanceToTarget < maxTargetDistance)
+                {
+                    // Check if target is infront of player
+                    if(Vector3.Dot(target.transform.position - transform.position, transform.forward) > 0.0f)
+                    {
+                        shellMoveComp.Target = target;
+                    }
+                }
+                break;
+        }
     }
 }
